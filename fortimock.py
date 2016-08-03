bcf7f376e37ed4ab006ee852d5094044560f0693
sheltont <shelton.ms@gmail.com>
2016-08-03 16:43:53 +0800

    完成基本功能，还需要努力： 1. 虚拟化 2. 前面放置nginx

---
+++ fortimock.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, make_response, redirect, session
+import requests
+import logging
+from urlparse import urlparse
+from logging.handlers import RotatingFileHandler
+
+app = Flask(__name__)
+
+# Generated with: import os; os.urandom(24)
+app.secret_key = '\xf7h\xcf\x9c\xf9=\xbc\x11\x7f+\xe7g\x1cm\xafo,ICWc\x12\xe6\xff'
+
+
+# http://127.0.0.1/?login&post=http://127.0.0.1:1000/fgtauth&magic=04050f8ebce39071&usermac=6c:40:08:bb:8a:5e&apmac=08:5b:0e:ec:74:be&apip=192.168.0.181&userip=192.168.10.4&device_type=mac
+
+# http://192.168.9.107/?login&
+# post=http://192.168.10.1:1000/fgtauth&
+# magic=0206038ec0e86a2b&usermac=a4:5e:60:dc:ed:a1&
+# apmac=08:5b:0e:ec:74:be&apip=192.168.0.181&
+# userip=192.168.10.7&device_type=mac
+
+# http://192.168.9.107/?auth=failed
+
+@app.route('/fgtauth', methods=['POST'])
+def fgtauth():
+    username = request.form.get('username', None)
+    password = request.form.get('password', None)
+    referer = request.headers.get('Referer', None)
+    _4Tredir = request.form.get('4Tredir', 'http://www.binq.com')
+
+    if username != 'binqsoft' or password != 'Password01!':
+        return redirect(get_redir_url(referer), code=302)
+
+    return make_response(render_template('success.html', redir=_4Tredir))
+
+
+
+def get_redir_url(url):
+    o = urlparse(url)
+    if o.port == None:
+        return "{0}://{1}/?Auth=Failed".format(o.scheme, o.hostname)
+    return "{0}://{1}:{2}/?Auth=Failed".format(o.scheme, o.hostname, o.port)
+
+
+if __name__ == '__main__':
+    handler = RotatingFileHandler('./logging/fortimock.log', maxBytes=1000000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    app.logger.info("Fortinet Mock Authenticator is running")
+    app.run(host='0.0.0.0', port=1000, debug=True)
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
-
 from flask import Flask, render_template, request, make_response, redirect, session
+from flask.ext.redis import FlaskRedis
+import sys
 import base64
 import datetime
 import requests
 import time
-
+import logging
+import cPickle as pickle
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+redis_store =  FlaskRedis(app)
 
 # Generated with: import os; os.urandom(24)
 app.secret_key = '\xf7h\xcf\x9c\xf9=\xbc\x11\x7f+\xe7g\x1cm\xafo,ICWc\x12\xe6\xff'
 
 
-VERY = 'http://127.0.0.1:5100/'
 # VERY = 'http://192.168.10.1:1000/fgtauth&'
-COOKIE_KEEP = 30
-REDIRECT_URL = 'http://192.168.9.107:5100/success'
+daysOfTTL = 3
 
 
 # http://192.168.9.107/?login&
@@ -25,37 +29,167 @@ REDIRECT_URL = 'http://192.168.9.107:5100/success'
 # apmac=08:5b:0e:ec:74:be&apip=192.168.0.181&
 # userip=192.168.10.7&device_type=mac
 
+# http://192.168.9.107/?auth=failed
+
 @app.route('/', methods=['GET'])
-def login():
-    print request.query_string
-    post_url = request.args.get('post') if request.args.get('post') else VERY
-    magic = request.args.get('magic') if request.args.get('magic') else 'jdhsjdhs'
-    username = request.cookies.get('username')
-    password = request.cookies.get('password')
+def default():
+    """
+    Flask 请求处理入口
+    """
+    app.logger.debug(request.args)
+    if request.args.has_key('Auth'):
+        return handle_auth()
+    return handle_login()
+
+
+def handle_auth():
+    """处理验证失败的结果
+    测试发现，如果验证没有通过，比如用户输入不正确的用户名或者密码
+    Fortinet会再次将用户重定向到Portal，并且请求的参数是?auth=failed
+    """
+    app.logger.info('handle_auth: ' + request.query_string)
+    usermac = get_usermac()
+    status = request.args.get('auth')
+    if status and status.lower() == 'failed':
+        if (usermac in session):
+            redis_store.delete(usermac)
+
+    # 将用户重定向到登录页面
+    resp = make_response(render_template('login.html'))
+    return resp
+
+def handle_login():
+    """处理登录请求页面
+    Fortinet将用户重定向到这个页面，返回表单，收集用户名、密码、是否保留密码等信息
+    """
+    # 将Fortinet传入的所有信息保存到Session中
+    app.logger.info('handle_login: ' + request.query_string)
+    usermac = request.args.get('usermac')
+    if usermac == None:
+        return make_response(render_template('error.html', 
+            errorinfo=u"Fortinet未传入UserMac参数，验证无法继续，请联系管理员"))   
+    usermac = usermac.lower()
+    session['usermac'] = usermac
+    session['post'] = request.args.get('post')
+    session['magic'] = request.args.get('magic')
+    session['apmac'] = request.args.get('apmac')
+    session['apip'] = request.args.get('apip')
+    session['userip'] = request.args.get('userip')
+    session['4Tredir'] = None
+    session['device_type'] = request.args.get('device_type')
+    
+    # 获取缓存的用户名和密码
+    userinfo = get_authinfo_from_cache(usermac)
+    if userinfo == None:
+        return make_response(render_template('login.html', 
+            username="", 
+            password="", 
+            ttl_hour = get_ttl_hour()))
+
+    return make_response(render_template('auth.html', 
+            username=userinfo['username'], 
+            password=userinfo['password'], 
+            post=session['post'], 
+            magic=session['magic'], 
+            _4Tredir=session['4Tredir']))
+
+
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    """保存用户提交的验证信息
+    保存用户验证信息，返回自动登录脚本页面
+    """
+    app.logger.info('auth: ' + request.query_string)
+    username = request.form['username']
+    password = request.form['password']
+    checked = request.form['checked']
+    print "checked " + checked
+    usermac = session['usermac']
+    post = session['post']
+    magic = session['magic']
+    _4Tredir = session['4Tredir']
+    app.logger.info('username: {0}, password: {1} chars'.format(username, len(password)))
     if username and password:
-        if 'visited_this_page' in session:
-            resp = make_response(render_template('login.html', post_url=post_url, magic=magic))
-            resp.set_cookie('username', '', expires=0)
-            resp.set_cookie('password', '', expires=0)
-            return resp
-        else:
-            session['visited_this_page'] = True
-            resp = make_response(render_template('login.html', post_url=post_url, magic=magic))
-            expire_date = datetime.datetime.now()
-            expire_date = expire_date + datetime.timedelta(days=COOKIE_KEEP)
-            resp.set_cookie('username', username, expires=expire_date)
-            resp.set_cookie('password', password, expires=expire_date)
-            log_content = username + ' try login at ' + datetime.datetime.now().strftime('%Y-%b-%d %H:%M:%S')
-            write_log(log_content)
-            return resp
-    return render_template('login.html', post_url=post_url, magic=magic)
+        if checked: set_authinfo_to_cache(usermac, username, password, get_ttl())
+        return make_response(render_template('auth.html', 
+            username=username, 
+            password=password, 
+            post=post, 
+            magic=magic, 
+            _4Tredir=_4Tredir))
+    # 如果用户名或者密码是空，重新收集信息
+    return make_response(render_template('login.html', username=username, password=password))
 
 
-def write_log(content):
-    file_name = "logging/" + time.strftime('%Y-%m-%d', time.localtime()) + '.txt'
-    with open(file_name, 'a+') as file:
-        file.write(content + '\n')
+@app.route('/clear', methods=['GET', 'POST'])
+def clear():
+    """清除某个MAC地址用户的缓存信息
+    特殊情况下，需要获取某个用户设备的Mac地址信息，从缓存中清除验证信息
+    """
+
+    app.logger.info('clear: ' + request.query_string)
+    count = 0
+    usermac = request.args.get('usermac')
+    if usermac: usermac = usermac.lower()
+    if usermac and redis_store.delete(usermac):
+        count += 1
+    return render_template('clear.html', usermac=usermac, count=count)
+
+
+def get_authinfo_from_cache(usermac):
+    """从缓存中获取验证信息
+    """
+    username = ""
+    password = ""
+    userdata = redis_store.get(usermac)
+    if userdata:
+        userinfo = pickle.loads(userdata)
+        username = userinfo['username']
+        password = userinfo['password']
+        return {'username': username, 'password': password}
+    return None
+
+def set_authinfo_to_cache(usermac, username, password, ttl):
+    userinfo = {'username': username, 'password': password, 'usermac': usermac}
+    userdata = pickle.dumps(userinfo)
+    app.logger.info("Cache the auth info for {0}:{1}".format(username, usermac))
+    redis_store.set(usermac, userdata, ttl)
+
+
+def get_usermac():
+    mac = session['usermac']
+    if mac:
+        return mac.lower()
+    return None
+
+
+def get_ttl():
+    return (daysOfTTL * 24 * 3600)
+
+def get_ttl_hour():
+    return daysOfTTL * 24
+
+
+def days_of_ttl(arg):
+    try:
+        var = int(arg)
+        return min(1, var)
+    except Exception:
+        return 3
 
 if __name__ == '__main__':
+    # The default authentcation TTL is 3 days.
+    print len(sys.argv)
+    print sys.argv
+    if len(sys.argv) >= 2:
+        daysOfTTL = days_of_ttl(sys.argv[1])
+
+    handler = RotatingFileHandler('./logging/bqauth.log', maxBytes=1000000, backupCount=7)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    app.logger.info("Binqsoft authentication portal is running")
+    app.logger.info("The default TTL of authentication session is {0} days".format(daysOfTTL))
     app.run(host='0.0.0.0', port=80, debug=True)
-    #app.run(debug=True)
