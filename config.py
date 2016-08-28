f93a51d49990bbdd85589a7b6ace0d9e8ae788f8
sheltont <shelton.ms@gmail.com>
2016-08-28 18:00:34 +0800

    Enable db link

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 
 import os
+from urlparse import urlparse
 
 host=os.getenv('BQAUTH_REDIS_HOST', 'localhost')
 port = os.getenv('BQAUTH_REDIS_PORT', '6379')
+url = os.getenv('DB_PORT')
+if url:
+	o = urlparse(url)
+	host = o.hostname
+	port = o.port
 
 REDIS_URL = "redis://{0}:{1}/0".format(host, port)
